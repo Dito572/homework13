058ca3a593837f3ac0e17a7acb903a39fd21704a
Dito572 <83370275+Dito572@users.noreply.github.com>
2022-04-04 17:31:23 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+let myName = "dito";
+let myHobby = "playing basketball";
+let currentYear = 2022;
+let yearOfBirth = 2002;
+let isStudent = true;
+let isEmployed = true;
+let myUndefined;
+let myNull = null;
+
+// console.log(myName, myHobby)
+
+// console.log(typeof myName, typeof currentYear, typeof isStudent, typeof myNull, typeof myUndefined);
+
+
+ let myAge = currentYear - yearOfBirth;
+
+
+console.log (" My name is " + myName + " I am " + myAge + " years old and my hobby is " + myHobby );
+
+let myText = `'My name is ${myName} I am ${myAge} years old and my hobby is ${myHobby}'`
+
+// console.log(myText)
+
+
